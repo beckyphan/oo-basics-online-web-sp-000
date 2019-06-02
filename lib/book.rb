128401eeb9179006de602ea222f7afa39e39b7ec
Becky Phan <beckyphan@live.com>
2019-06-02 14:20:36 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -7,7 +7,7 @@ class Book
   attr_accessor :title, :author, :page_count, :genre
   
   def turn_page
-    puts "Flipping the page...wow, you readfast!"
+    puts "Flipping the page...wow, you read fast!"
   end
     
 end
